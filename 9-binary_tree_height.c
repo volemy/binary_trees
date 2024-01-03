e4042d0c8910517667626e6abe22104d8cedc3dd
volemy <minayobenevolence0@gmail.com>
2024-01-03 11:59:24 +0000

    changes

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,11 +13,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (!tree)
 		return (0);
+
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	if (left > right)
-		return (left + 1);
-	else
-		return (right + 1);
+	return (left > right ? left + 1 : right + 1);
 }
