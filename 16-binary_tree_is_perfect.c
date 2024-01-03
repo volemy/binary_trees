5a9714eb5cac53312ac9b94835a9b7bf38984238
volemy <minayobenevolence0@gmail.com>
2024-01-03 12:20:55 +0000

    changes

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,21 +6,25 @@
  *
  * Return: Height of the tree
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	size_t left, right;
 
-	if (!tree)
+	if (tree == NULL)
+	{
 		return (0);
-	left = binary_tree_height(tree->left);
-	right = binary_tree_height(tree->right);
+	}
 
-	if (left > right)
-		return (left + 1);
+	if (tree->left)
+		left = 1 + binary_tree_height(tree->left);
 	else
-		return (right + 1);
+		left = 0;
 
+	if (tree->right)
+		right = 1 + binary_tree_height(tree->right);
+	else
+		right = 0;
+	return (left > right ? left : right);
 }
 
 /**
