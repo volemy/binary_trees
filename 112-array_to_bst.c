8eb7369e95b6c576b22de9c2157fb7076e370b87
Brokeice123 <keithmarkomen@gmail.com>
2024-01-04 19:25:17 +0000

    a function that builds a Binary Search Tree from an array

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -16,9 +16,6 @@ bst_t *array_to_bst(int *array, size_t size)
 	if (!array || !size)
 		return (NULL);
 	for (i = 0; i < size; i++)
-	{
-		if (!bst_insert(&root, array[i]))
-			return (NULL);
-	}
+		bst_insert(&root, array[i]);
 	return (root);
 }
